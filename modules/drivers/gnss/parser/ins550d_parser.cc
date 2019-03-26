573aec826ce68701bb2131e412dbcdf409f9575a
kingwan560 <kingwan560@163.com>
2019-03-26 09:38:33 +0800

    修改惯导解析程序 在Ins消息中添加惯导状态字段

--- modules/drivers/gnss/parser/ins550d_parser.cc
+++ modules/drivers/gnss/parser/ins550d_parser.cc
@@ -143,7 +143,8 @@ Parser::MessageType INS550DParser::GetMessage(MessagePtr *message_ptr) {
 bool INS550DParser::HandleIns() {
 int DataLen = 58;
 uint8_t checksum = 0;
-uint32_t buf;
+int16_t buf;
+int32_t buf32;
 
 if (data_[0] == 0xBD && data_[1] == 0xDB && data_[2] == 0x0B)
 {
@@ -155,55 +156,61 @@ if (data_[0] == 0xBD && data_[1] == 0xDB && data_[2] == 0x0B)
 
     if (data_[DataLen - 1] == checksum)
     {
-        buf = (uint32_t)(data_[3] + (data_[4] << 8));
-        double roll = ((double)buf) * 360 / 32768;  // 横滚角
+        buf = (int16_t)(data_[3] + (data_[4] << 8));
+        double roll = ((double)buf/32768) * 360;  // 横滚角
 
-        buf = (uint32_t)(data_[5] + (data_[6] << 8));
-        double pitch = ((double)buf) * 360 / 32768; // 俯仰角
+        buf = (int16_t)(data_[5] + (data_[6] << 8));
+        double pitch = ((double)buf/32768) * 360; // 俯仰角
 
-        buf = (uint32_t)(data_[7] + (data_[8] << 8));
-        double yaw = ((double)buf) * 360 / 32768;   // 方位角
-
-        /*****************************************************/
-        buf = (uint32_t)(data_[9] + (data_[10] << 8));
-        double gx = ((double)buf) * 300 / 32768;    // 陀螺 x 轴
-
-        buf = (uint32_t)(data_[11] + (data_[12] << 8));
-        double gy = ((double)buf) * 300 / 32768;    // 陀螺 y 轴
-
-        buf = (uint32_t)(data_[13] + (data_[14] << 8));
-        double gz = ((double)buf) * 300 / 32768;    // 陀螺 z 轴
+        buf = (int16_t)(data_[7] + (data_[8] << 8));
+        double yaw = ((double)buf/32768) * 360;   // 方位角
 
         /*****************************************************/
-        buf = (uint32_t)(data_[15] + (data_[16] << 8));
-        double ax = ((double)buf) * 12 / 32768;   // 加表 x 轴
+        buf = (int16_t)(data_[9] + (data_[10] << 8));
+        double gx = ((double)buf/32768) * 300;    // 陀螺 x 轴
 
-        buf = (uint32_t)(data_[17] + (data_[18] << 8));
-        double ay = ((double)buf) * 12 / 32768;   // 加表 y 轴
+        buf = (int16_t)(data_[11] + (data_[12] << 8));
+        double gy = ((double)buf/32768) * 300;    // 陀螺 y 轴
 
-        buf = (uint32_t)(data_[19] + (data_[20] << 8));
-        double az = ((double)buf) * 12 / 32768;   // 加表 z 轴
-
-        /*****************************************************/
-        buf = (uint32_t)(data_[21] + (data_[22] << 8) + (data_[23] << 16) + (data_[24] << 24));
-        double lat = ((double)buf) / 1e7f;   // 纬度
-
-        buf = (uint32_t)(data_[25] + (data_[26] << 8) + (data_[27] << 16) + (data_[28] << 24));
-        double lon = ((double)buf) / 1e7f;   // 经度
-
-        buf = (uint32_t)(data_[29] + (data_[30] << 8) + (data_[31] << 16) + (data_[32] << 24));
-        double alt = ((double)buf) / 1e3f;    // 高度
+        buf = (int16_t)(data_[13] + (data_[14] << 8));
+        double gz = ((double)buf/32768) * 300;    // 陀螺 z 轴
 
         /*****************************************************/
-        buf = (uint32_t)(data_[33] + (data_[34] << 8));
-        double north_velocity = ((double)buf) * 100/ 32768; // 北向速度
-        buf = (uint32_t)(data_[35] + (data_[36] << 8));
-        double east_velocity = ((double)buf) * 100/ 32768; // 东向速度
-        buf = (uint32_t)(data_[37] + (data_[38] << 8));
-        double up_velocity = ((double)buf) * 100/ 32768; // 地向速度
+        buf = (int16_t)(data_[15] + (data_[16] << 8));
+        double ax = ((double)buf/32768) * 12;   // 加表 x 轴
+
+        buf = (int16_t)(data_[17] + (data_[18] << 8));
+        double ay = ((double)buf/32768) * 12;   // 加表 y 轴
+
+        buf = (int16_t)(data_[19] + (data_[20] << 8));
+        double az = ((double)buf/32768) * 12;   // 加表 z 轴
+
+        /*****************************************************/
+        buf32 = (int32_t)(data_[21] + (data_[22] << 8) + (data_[23] << 16) + (data_[24] << 24));
+        double lat = ((double)buf32) / 1e7f;   // 纬度
+
+        buf32 = (int32_t)(data_[25] + (data_[26] << 8) + (data_[27] << 16) + (data_[28] << 24));
+        double lon = ((double)buf32) / 1e7f;   // 经度
+
+        buf32 = (int32_t)(data_[29] + (data_[30] << 8) + (data_[31] << 16) + (data_[32] << 24));
+        double alt = ((double)buf32) / 1e3f;    // 高度
+
+        /*****************************************************/
+        buf = (int16_t)(data_[33] + (data_[34] << 8));
+        double north_velocity = ((double)buf/32768) * 100; // 北向速度
+        buf = (int16_t)(data_[35] + (data_[36] << 8));
+        double east_velocity = ((double)buf/32768) * 100; // 东向速度
+        buf = (int16_t)(data_[37] + (data_[38] << 8));
+        double up_velocity = ((double)buf/32768) * 100; // 地向速度
 
 
-        // uint8_t State = data_[39];    // 初校准状态
+        uint8_t state = data_[39];    // 初校准状态
+
+        /*****************************************************/
+
+        int16_t data1 = (int16_t)(data_[46] + (data_[47] << 8));
+        int16_t data2 = (int16_t)(data_[48] + (data_[49] << 8));
+        int16_t data3 = (int16_t)(data_[50] + (data_[51] << 8));
 
         // buf = (uint32_t)(data_[46] + (data_[47] << 8));
         // double Temp = ((double)buf) * 200 / 32768;    // 温度
@@ -211,7 +218,7 @@ if (data_[0] == 0xBD && data_[1] == 0xDB && data_[2] == 0x0B)
         // buf = (uint32_t)(data_[52] + (data_[53] << 8) + (data_[54] << 16) + (data_[55] << 24));
         // double time = ((double)buf) / 4000;    // 时间
 
-        // uint8_t datatype = data_[56];   // 轮循数据类型
+        uint8_t datatype = data_[56];   // 轮循数据类型
 
         /*****************************************************/
         ins_.mutable_header()->set_timestamp_sec(cyber::Time::Now().ToSecond());
@@ -222,9 +229,9 @@ if (data_[0] == 0xBD && data_[1] == 0xDB && data_[2] == 0x0B)
         ins_.mutable_position()->set_lat(lat);
         ins_.mutable_position()->set_height(alt);
 
-        ins_.mutable_euler_angles()->set_x(roll);
+        ins_.mutable_euler_angles()->set_x(yaw);
         ins_.mutable_euler_angles()->set_y(pitch);
-        ins_.mutable_euler_angles()->set_z(yaw);
+        ins_.mutable_euler_angles()->set_z(roll);
    
         ins_.mutable_linear_velocity()->set_x(east_velocity);
         ins_.mutable_linear_velocity()->set_y(north_velocity);
@@ -237,6 +244,15 @@ if (data_[0] == 0xBD && data_[1] == 0xDB && data_[2] == 0x0B)
         ins_.mutable_linear_acceleration()->set_x(ax);
         ins_.mutable_linear_acceleration()->set_y(ay);
         ins_.mutable_linear_acceleration()->set_z(az);
+
+        ins_.set_state(state);  // 初校准状态
+
+        if(datatype==32)    // GPS 状态
+        {
+          ins_.set_positionstate(data1);// GPS 定位状态
+          ins_.set_numstars(data2);     // 收星数
+          ins_.set_directionalstate(data3); //GPS 定向状态
+        }
 
         return true;
         
